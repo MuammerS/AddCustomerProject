41a8efa85dc85f5d5fe4f75e184e7e635d0a3b09
MuammerS <muammersonmez005@yahoo.com>
2021-01-23 21:28:50 +0300

    Add project files.

---
+++ AddCustomerProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DictionaryKavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> CustomerNameCustomerNumber = new Dictionary<string, int>();
+                                        //Name     //Number
+                                        //İsim     //Numara
+            CustomerNameCustomerNumber.Add("Mummer", 1453);
+            CustomerNameCustomerNumber.Add("Turan", 2000);
+            CustomerNameCustomerNumber.Add("Egemen", 1905);
+            CustomerNameCustomerNumber.Add("Ayşe", 1907);
+            CustomerNameCustomerNumber.Add("Sıla", 2003);
+
+            foreach (var degeroku in CustomerNameCustomerNumber)
+            {
+                Console.WriteLine(degeroku);
+            }
+            Console.ReadLine();
+        }
+    }
+}
